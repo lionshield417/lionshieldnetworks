c02bd166cbd782ab921ddb0a1e89de23ca226813
Jovany Soto <jsoto@lionshieldnetworks.com>
2026-08-10 02:41:58 -0400

    Exclude client portal from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://lionshieldnetworks.com',
 
 	integrations: [
 		sitemap({
-			filter: (page) =>
-				page !== 'https://lionshieldnetworks.com/client-portal/',
+			filter: (page) => !page.includes('/client-portal'),
 		}),
 	],
 
